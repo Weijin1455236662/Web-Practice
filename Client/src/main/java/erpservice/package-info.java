90f0dea2ea805b7e81bb72101a3d9e94cf6b363e
weijin <1455236662@qq.com>
2020-09-22 11:11:10 +0800

    add: Client

---
+++ Client/src/main/java/erpservice/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://ERPService/")
+package erpservice;
--- LegacySystem/src/main/java/Application.java
+++ LegacySystem/src/main/java/Application.java
@@ -1,11 +1,11 @@
-import ERPService.ResourceService;
+import ERPService.Resource;
 
 import javax.xml.ws.Endpoint;
 
 public class Application {
     public static void main(String[] args) {
         String address="http://localhost:8080/resourceService";
-        Endpoint.publish(address,new ResourceService());
+        Endpoint.publish(address,new Resource());
         System.out.println("ResourceService Publishes Success!");
         System.out.println("Address:" + address);
     }
