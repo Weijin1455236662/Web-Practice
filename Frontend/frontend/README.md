69270d37cacf5a2ec7b8c0f8ab763bfb773961fe
weijin <1455236662@qq.com>
2020-10-16 15:46:42 +0800

    fix: lower Vue version

--- Frontend/frontend/README.md
+++ Frontend/frontend/README.md
@@ -1,4 +1,4 @@
-# frontend
+# pro
 
 ## Project setup
 ```
--- Frontend/frontend/src/main.js
+++ Frontend/frontend/src/main.js
@@ -1,7 +1,23 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import VueFusionCharts from 'vue-fusioncharts';
+import FusionCharts from 'fusioncharts/core';
+// import Column2D from 'fusioncharts/fusioncharts.charts';
+import gantt from 'fusioncharts/viz/gantt'
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+import Charts from "fusioncharts/fusioncharts.charts";
 
-createApp(App).use(store).use(router).mount('#app')
+Vue.use(VueFusionCharts, FusionCharts, FusionTheme,gantt);
+// Resolves charts dependency
+Charts(FusionCharts);
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
