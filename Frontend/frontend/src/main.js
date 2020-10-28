d86421b1a1db8b5d48368dadbce8bc22d7119648
zhaoxingrui <171250598@smail.nju.edu.cn>
2020-10-28 17:15:56 +0800

    add:  orderGantt punctuality

--- Frontend/frontend/src/main.js
+++ Frontend/frontend/src/main.js
@@ -6,11 +6,12 @@ import store from './store'
 import VueFusionCharts from 'vue-fusioncharts';
 import FusionCharts from 'fusioncharts/core';
 import gantt from 'fusioncharts/viz/gantt'
+import angulargauge from 'fusioncharts/viz/angulargauge'
 import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
 import Charts from "fusioncharts/fusioncharts.charts";
 import './assets/icon/iconfont-embedded.css'
 
-Vue.use(VueFusionCharts, FusionCharts, FusionTheme,gantt);
+Vue.use(VueFusionCharts, FusionCharts, FusionTheme,gantt, angulargauge);
 // Resolves charts dependency
 Charts(FusionCharts);
 
--- Frontend/frontend/src/views/OrderGantt.vue
+++ Frontend/frontend/src/views/OrderGantt.vue
@@ -1,15 +1,106 @@
 <template>
-  <div>
-    <h1>订单甘特图</h1>
+  <div class="main">
+    <div class="head">
+      <div class="text">订单甘特图</div>
+      <hr/>
+    </div>
+    <div class="container">
+      <!-- <div class="today">
+        今天是{{today}}
+      </div> -->
+      <fusioncharts
+        :type="type"
+        :width="width"
+        :height="height"
+        :dataFormat="dataFormat"
+        :dataSource="dataSource"
+      ></fusioncharts>
+    </div>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "OrderGantt"
+export default {
+  name: "OrderGantt",
+  data(){
+    return{
+      today: "",
+      type: "angulargauge",
+      width: "50%",
+      height: "40%",
+      dataFormat: "json",
+      dataSource: {},
+      punctuality: "30"
     }
+  },
+  mounted(){
+    let nowDate = new Date()
+    let date = {
+      year: nowDate.getFullYear(),
+      month: nowDate.getMonth() + 1,
+      day: nowDate.getDate()
+    }
+    this.today = date.year + "年" + date.month + "月" + date.day + "日"
+    this.dataSource = {
+      chart: {
+        caption: "按期交货率",
+        subcaption: this.today + "之前",
+        showvalue: "1",
+        numbersuffix: "%",
+        theme: "fusion",
+      },
+      colorrange: {
+        color: [
+          {
+            minvalue: "0",
+            maxvalue: "25",
+            code: "#F2726F"
+          },
+          {
+            minvalue: "25",
+            maxvalue: "50",
+            code: "#AB05FA"
+          },
+          {
+            minvalue: "50",
+            maxvalue: "75",
+            code: "#FFC533"
+          },
+          {
+            minvalue: "75",
+            maxvalue: "100",
+            code: "#62B58F"
+          },
+        ]
+      },
+      dials: {
+        dial: [
+          {
+            value: this.punctuality
+          }
+        ]
+      }
+    }
+  }
+}
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+.main{
+  .head{
+    margin: 30px 5% 0;
+    text-align: left;
+    .text{
+      font-size: 32px;
+      font-weight: bold;
+    }
+  }
+  .container{
+    margin: 30px 5% 0;
+    .today{
+      text-align: left;
+      font-size: 30px;
+    }
+  }
+}
 </style>
