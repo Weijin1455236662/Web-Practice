936e5626f7630d39414d1a2516ec4528489854b4
zhaoxingrui <171250598@smail.nju.edu.cn>
2020-10-29 10:56:13 +0800

    update: orderGantt completed

--- Frontend/frontend/src/main.js
+++ Frontend/frontend/src/main.js
@@ -7,11 +7,12 @@ import VueFusionCharts from 'vue-fusioncharts';
 import FusionCharts from 'fusioncharts/core';
 import gantt from 'fusioncharts/viz/gantt'
 import angulargauge from 'fusioncharts/viz/angulargauge'
+import msbar2d from 'fusioncharts/viz/msbar2d'
 import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
 import Charts from "fusioncharts/fusioncharts.charts";
 import './assets/icon/iconfont-embedded.css'
 
-Vue.use(VueFusionCharts, FusionCharts, FusionTheme,gantt, angulargauge);
+Vue.use(VueFusionCharts, FusionCharts, FusionTheme,gantt, angulargauge, msbar2d);
 // Resolves charts dependency
 Charts(FusionCharts);
 
--- Frontend/frontend/src/views/OrderGantt.vue
+++ Frontend/frontend/src/views/OrderGantt.vue
@@ -5,15 +5,22 @@
       <hr/>
     </div>
     <div class="container">
-      <!-- <div class="today">
-        今天是{{today}}
-      </div> -->
       <fusioncharts
-        :type="type"
-        :width="width"
-        :height="height"
-        :dataFormat="dataFormat"
-        :dataSource="dataSource"
+        :type="chart1.type"
+        :width="chart1.width"
+        :height="chart1.height"
+        :dataFormat="chart1.dataFormat"
+        :dataSource="chart1.dataSource"
+      ></fusioncharts>
+      <div class="today">
+        今天是 <span class="text">{{today1}}</span>
+      </div>
+      <fusioncharts
+        :type="chart2.type"
+        :width="chart2.width"
+        :height="chart2.height"
+        :dataFormat="chart2.dataFormat"
+        :dataSource="chart2.dataSource"
       ></fusioncharts>
     </div>
   </div>
@@ -25,12 +32,22 @@ export default {
   data(){
     return{
       today: "",
-      type: "angulargauge",
-      width: "50%",
-      height: "40%",
-      dataFormat: "json",
-      dataSource: {},
-      punctuality: "30"
+      today1: "",
+      chart1: {
+        type: "angulargauge",
+        width: "40%",
+        height: "30%",
+        dataFormat: "json",
+        dataSource: {},
+        punctuality: "30"
+      },
+      chart2: {
+        type: "msbar2d",
+        width: "100%",
+        height: "2000",
+        dataFormat: "json",
+        dataSource: {},
+      }
     }
   },
   mounted(){
@@ -41,7 +58,9 @@ export default {
       day: nowDate.getDate()
     }
     this.today = date.year + "年" + date.month + "月" + date.day + "日"
-    this.dataSource = {
+    this.today1 = date.year + "-" + date.month + "-" + date.day
+    // 绘制按期交货率图
+    this.chart1.dataSource = {
       chart: {
         caption: "按期交货率",
         subcaption: this.today + "之前",
@@ -76,10 +95,160 @@ export default {
       dials: {
         dial: [
           {
-            value: this.punctuality
+            value: this.chart1.punctuality
           }
         ]
       }
+    }
+    // 绘制订单甘特图
+    this.chart2.dataSource = {
+      chart: {
+        caption: "订单进度",
+        captionFontSize: 24,
+        // paletteColors: "#FF0000, #0372AB, #FF5904",
+        xAxisname: "订<br>单<br>号",
+        xAxisNameFontSize: 20,
+        rotateXAxisName: 0,
+        yAxisMaxValue: 100,
+        legendpostion: "top",
+        showValues: 1,
+        numberSuffix: "%",
+        plottooltext: "订单 $label<br>" + "$seriesName：$value",
+        theme: "fusion",
+      },
+      categories: [
+        {
+          fontSize: 18,
+          toolText: "$label<br>预计完成：2020/10/28<br>实际完成：2020/10/25",
+          category: [
+            {
+              label: "418575"
+            },
+            {
+              label: "418577"
+            },
+            {
+              label: "764486"
+            },
+            {
+              label: "762904"
+            },
+            {
+              label: "418477"
+            },
+            {
+              label: "418006"
+            },
+            {
+              label: "418575",
+            },
+            {
+              label: "418577"
+            },
+            {
+              label: "764486"
+            },
+            {
+              label: "762904"
+            },
+            {
+              label: "418477"
+            },
+            {
+              label: "418006"
+            },
+            {
+              label: "418575",
+            },
+            {
+              label: "418577"
+            },
+            {
+              label: "764486"
+            },
+            {
+              label: "762904"
+            },
+            {
+              label: "418477"
+            },
+            {
+              label: "418006"
+            }
+          ]
+        }
+      ],
+      dataset: [
+        {
+          seriesName: "装配率",
+          data: [
+            {
+              value: "100%",
+            },
+            {
+              value: "80%"
+            },
+            {
+              value: "60%"
+            },
+            {
+              value: "10%"
+            },
+            {
+              value: "13%"
+            },
+            {
+              value: "40%"
+            }
+          ]
+        },
+        {
+          seriesName: "打弹片率",
+          data: [
+            {
+              value: "80%"
+            },
+            {
+              value: "54%"
+            },
+            {
+              value: "20%"
+            },
+            {
+              value: "50%"
+            },
+            {
+              value: "0%"
+            },
+            {
+              value: "40%"
+            }
+          ]
+        },
+        {
+          seriesName: "测试率",
+          data: [
+            {
+              value: "80%"
+            },
+            {
+              value: "54%"
+            },
+            {
+              value: "20%"
+            },
+            {
+              value: "50%"
+            },
+            {
+              value: "73%"
+            },
+            {
+              value: "40%"
+            }
+          ]
+        }
+      ]
     }
   }
 }
@@ -99,7 +268,10 @@ export default {
     margin: 30px 5% 0;
     .today{
       text-align: left;
-      font-size: 30px;
+      font-size: 26px;
+      .text{
+        font-weight: bold;
+      }
     }
   }
 }
